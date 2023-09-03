d77cac3689b2462c7f3c8f27abbe90142c34418e
Jeferson Melo <jeferosnmedeiros000@gmail.com>
2023-09-03 11:00:05 -0300

    Commit inicial do Projeto uniquiz
    Estrutura Django
    
    Signed-off-by: Jeferson Melo <jeferosnmedeiros000@gmail.com>

---
+++ uniquiz/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from uniquiz.users.models import User
+from uniquiz.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
